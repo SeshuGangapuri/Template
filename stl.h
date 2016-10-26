cc56d4a53a3aa8e8301abfc15da3b3d3fe245ce0
MadhuBabu <madhubabu@nes.co.in>
2016-10-26 22:53:10 +0530

    second commit

---
+++ stl.h
@@ -0,0 +1,18 @@
+#ifndef STL_H
+#define STL_H
+
+#include <QObject>
+#include <Qobject>
+
+class STL : public QObject
+{
+    Q_OBJECT
+public:
+    explicit STL(QObject *parent = 0);
+
+signals:
+
+public slots:
+};
+
+#endif // STL_H
